4a925ac32a358a5987bbb8b95666b2f2001c6f67
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-01 17:13:12 -0700

    built reducer

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,7 +1,41 @@
-export const initialState = {};
+import {
+  FETCHING_SMURFS_START,
+  FETCHING_SMURFS_SUCCESS,
+  FETCHING_SMURFS_FAILURE,
+} from "../actions/actions";
+
+export const initialState = {
+  smurfs: [
+    {
+      name: "",
+      age: null,
+      height: "",
+      id: null,
+    },
+  ],
+  isFetching: false,
+  error: "",
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_SMURFS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        smurfs: action.payload,
+      };
+    case FETCHING_SMURFS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
