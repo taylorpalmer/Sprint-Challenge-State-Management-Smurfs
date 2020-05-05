ba0dbe3d44c0c983c0723e510f19ca36250be6f5
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-05 13:50:00 -0700

    Completed MVP, needs styling

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -11,7 +11,7 @@ const SmurfList = () => {
       {smurfs.map((smurf) => {
         return (
           <section className="smurf" key={smurf.id}>
-            <h2>{smurf.name}</h2>
+            <h2>{smurf.name} Smurf</h2>
             <p>Age: {smurf.age}</p>
             <p>Height: {smurf.height}</p>
           </section>
